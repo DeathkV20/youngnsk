117cb9b0f62492e96883a1cbbab71029252b8f98
DeathkV20 <deathkv2.0@gmail.com>
2020-06-16 18:58:02 +0300

    v2

--- assets/js/dots.js
+++ assets/js/dots.js
@@ -2,8 +2,10 @@ window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos > 7) {
       document.getElementById("dots").classList.add ("opacity")
+      document.getElementById("dier").classList.add ("slided")
     } else {
       document.getElementById("dots").classList.remove ("opacity")
+      document.getElementById("dier").classList.remove("slided")
     }
     prevScrollpos = currentScrollPos;
   }
